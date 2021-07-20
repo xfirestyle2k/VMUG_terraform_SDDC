d46f0394cd0362149d9ec3865cce72405e6d0e63
xfirestyle2k <firestyle2k@hotmail.com>
2021-07-20 10:52:41 +0200

    commit

--- vars.tf
+++ vars.tf
@@ -12,50 +12,50 @@ variable "aws_account_number" {
 
 variable "sddc_name"{
   description = "Name of SDDC."
-  default     = ""
+  default     = "VMUG_Terraform"
 }
 
 variable "sddc_region" {
   description = "The AWS  or VMC specific region."
-  default     = ""
+  default     = "eu-central-1"
 }
 
 variable "sddc_mgmt_subnet" {
   description = "SDDC management network CIDR. Only prefix of 16, 20 and 23 are supported."
-  default     = ""
+  default     = "10.2.0.0/16"
 }
 
 variable "sddc_client_net" {
   description = "A logical network segment that will be created with the SDDC under the compute gateway."
-  default     = ""
+  default     = "192.168.2.0/24"
 }
 
 variable host_instance_type {
   description = "The instance type for the ESX hosts in the primary cluster of the SDDC. Possible values: I3_METAL, R5_METAL."
-  default     = ""
+  default     = "I3_METAL"
 }
 
 variable sddc_num_hosts {
   description = "The number of hosts in SDDC."
-  default     = ""
+  default     = "3"
 }
 
 variable provider_type {
   description = "Determines what additional properties are available based on cloud provider. Default value : AWS"
-  default     = ""
+  default     = "ZEROCLOUD"
 }
 
 variable sddc_type {
   description = "Denotes the sddc type, if the value is null or empty, the type is considered as default. Possible values : '1NODE', 'DEFAULT'. "
-  default     = ""
+  default     = "DEFAULT"
 }
 
 variable size {
   description = "The size of the vCenter and NSX appliances. 'large' or 'LARGE' SDDC size corresponds to a large vCenter appliance and large NSX appliance. 'medium' or 'MEDIUM' SDDC size corresponds to medium vCenter appliance and medium NSX appliance. Default : 'medium'."
-  default     = "" 
+  default     = "medium" 
 }
 
 variable deployment_type {
   description = "Denotes if request is for a SingleAZ or a MultiAZ SDDC. Default : SingleAZ."
-  default     = ""
+  default     = "SingleAZ"
 }
